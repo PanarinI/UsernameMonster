804d46206d8d1e98c0c29f627e3920c67d803eb2
IG <panarin2005@yandex.ru>
2025-02-11 16:47:24 +0300

    DB initiated_before branching to DB

--- bot/main.py
+++ bot/main.py
@@ -6,6 +6,7 @@ from handlers.generate import generate_router
 from handlers.check import check_router
 from handlers.common import common_router
 from handlers.help import help_router
+from database.database import init_db
 
 # Настройка логирования
 from utils.logger import setup_logging
@@ -14,12 +15,15 @@ setup_logging()  # Вызов перед запуском бота
 
 async def main():
     """Запуск бота"""
+    # Проверяем инициализацию БД
+    await init_db()
     # Регистрируем обработчики
     dp.include_router(start_router)
     dp.include_router(help_router)
     dp.include_router(check_router)
     dp.include_router(generate_router)
     dp.include_router(common_router)
+
     logging.info("Бот запущен!")  # Логируем запуск
     # Запускаем polling
     await dp.start_polling(bot)
